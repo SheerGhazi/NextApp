e480eb61bc8416c7dd5c3b937188648e71d56c4e
SheerGhazi <sher.ghazi2192@gmail.com>
2024-12-14 22:35:02 +0500

    feat: profile.jsx

--- src/app/about/Page.js
+++ src/app/about/Page.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const about = () => {
+const About = () => {
   return (
     <div>
-      Hello about
+      About
     </div>
   )
 }
 
-export default about
+export default About;
--- src/app/profile/Page.js
+++ src/app/profile/Page.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const page = () => {
+const Profile = () => {
   return (
     <div>
-      Wellcome Home
+      Profile
     </div>
   )
 }
 
-export default page
+export default Profile;
